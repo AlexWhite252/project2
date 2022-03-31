18bd49accf4eaa3976c34b3568d2e6052e7b5acf
Caleb Reid <60555714+calebreid2829@users.noreply.github.com>
2022-03-31 17:19:43 -0400

    SparkQueries created

---
+++ src/main/scala/project2/SparkQueries.scala
@@ -0,0 +1,15 @@
+package project2
+
+import org.apache.spark.sql.SparkSession
+
+class SparkQueries(spark:SparkSession) {
+
+  //Create your querries in separate functions like this
+  //Alternatively name them like mynameQuery1,mynameQuery2 ect...
+  def Querry1(): Unit={
+
+  }
+  def Querry2(): Unit={}
+  //ect...
+}
+
--- src/main/scala/project2/project2.scala
+++ src/main/scala/project2/project2.scala
@@ -3,6 +3,7 @@ package project2
 import org.apache.spark.sql.SparkSession
 
 object project2{
+
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", "C:\\hadoop")
     val spark = SparkSession
@@ -14,10 +15,10 @@ object project2{
     println("Created Spark session\n")
     spark.sparkContext.setLogLevel("ERROR")
 
-    import spark.implicits._
+    val sparkqueries = new SparkQueries(spark)
+    sparkqueries.Querry1()
+    sparkqueries.Querry2()
 
-    val a = Seq(("hawaii", 50), ("brazil", 30)).toDF()
-    a.show()
   }
   //Sets up the spark environment
 }
