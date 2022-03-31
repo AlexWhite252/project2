a04b78985b945456d0f7f429c08a95b02ba956c7
Caleb Reid <60555714+calebreid2829@users.noreply.github.com>
2022-03-31 17:29:29 -0400

    updated documentation

--- src/main/scala/project2/SparkQueries.scala
+++ src/main/scala/project2/SparkQueries.scala
@@ -6,6 +6,7 @@ class SparkQueries(spark:SparkSession) {
 
   //Create your querries in separate functions like this
   //Alternatively name them like mynameQuery1,mynameQuery2 ect...
+  //Or just whatever type of analysis you're doing like AverageConfirmed or whatever
   def Querry1(): Unit={
 
   }
