e59fc02d564997eca3afb5db01e1ee6668cebcbb
Savannah Jones <savannah350@revature.net>
2022-04-04 17:48:39 -0400

    Notes for tomorrow

--- src/main/scala/project2/SparkQueries.scala
+++ src/main/scala/project2/SparkQueries.scala
@@ -23,17 +23,20 @@ class SparkQueries(spark:SparkSession) {
   //ect...
   def confirmedFirst(): Unit = {
     /*---Confirmed cases, deaths, recovered within FIRST 4 months---*/
+    /*---Change table to Month,Country,Confirmed,Deaths,Recovered---*/
     spark.sql("SELECT DISTINCT COUNT(Country_Region) AS Countries,SUM(Confirmed) AS Confirmed_Cases,SUM(Deaths) AS Deaths,SUM(Recovered) AS Recovered FROM covid19data WHERE ObservationDate BETWEEN '01/22/2020' AND '05/22/2020' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("data/ConfirmedFirst")
   }
   def confirmedLast(): Unit = {
     /*---Confirmed cases, deaths, recovered within LAST 4 months---*/
+    /*---Change table to Month,Country,Confirmed,Deaths,Recovered---*/
     spark.sql("SELECT DISTINCT COUNT(Country_Region) AS Countries,SUM(Confirmed) AS Confirmed_Cases,SUM(Deaths) AS Deaths,SUM(Recovered) AS Recovered FROM covid19data WHERE ObservationDate BETWEEN '01/02/2021' AND '05/02/2021' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("data/ConfirmedLast")
   }
   def confirmedChina(): Unit = {
-    /*---Confirmed in China vs rest of the world---*/
+    /*---Confirmed in China then other countries---*/
     spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Confirmed) AS Confirmed from covid19data where Country_Region == 'Mainland China' AND Last_Update='2021-05-03 04:20:39' GROUP BY Country UNION SELECT DISTINCT Country_Region as Country,SUM(Confirmed)as not from covid19data where Country_Region != 'Mainland China' AND Last_Update='2021-05-03 04:20:39' GROUP BY Country").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("src/main/Data/ConfirmedChina")
+    /*---Confirmed in China w/ country count confirmed---*/
     spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Confirmed) AS Confirmed from covid19data where Country_Region == 'Mainland China' AND Last_Update='2021-05-03 04:20:39' GROUP BY Country UNION SELECT DISTINCT COUNT(Country_Region) as Country,SUM(Confirmed)as not from covid19data where Country_Region != 'Mainland China' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("src/main/Data/ConfirmedChinaVsCount")
-
+    /*---Sample stat---*/
     val countries = spark.sql("SELECT DISTINCT COUNT(Country_Region) FROM covid19data WHERE Country_Region!='%China%'").head().getLong(0)
     val confirmed = spark.sql("SELECT SUM(Confirmed) FROM covid19data WHERE Last_Update='2021-05-03 04:20:39'").head().getDouble(0)
     val chinaCases = 1 - ((countries / confirmed) * 100)
@@ -41,6 +44,7 @@ class SparkQueries(spark:SparkSession) {
   }
   def topRecovered(): Unit = {
     /*---Top 10 recovered across countries---*/
+    /*---Might need to alter due to large numbers but I will look at/fix tomorrow (might not need SUM())?---*/
     spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Recovered) AS Recovered FROM covid19data WHERE Last_Update='2021-05-03 04:20:39' GROUP BY Country_Region ORDER BY Recovered DESC LIMIT 10").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("data/TopRecovered")
   }
   def bottomRecovered(): Unit = {
