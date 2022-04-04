fe6e558afb4e27f395f0fc1b71f0f2a399556722
Savannah Jones <savannah350@revature.net>
2022-04-04 11:08:53 -0400

    queries save to csv in src/main/Data

--- src/main/scala/project2/SparkQueries.scala
+++ src/main/scala/project2/SparkQueries.scala
@@ -13,5 +13,31 @@ class SparkQueries(spark:SparkSession) {
     df
   }
   //ect...
+  def queries(): Unit = {
+    /*---Confirmed cases, deaths, recovered within FIRST 4 months---*/
+    spark.sql("SELECT DISTINCT COUNT(Country_Region) AS Countries,SUM(Confirmed) AS Confirmed_Cases,SUM(Deaths) AS Deaths,SUM(Recovered) AS Recovered FROM covid19data WHERE ObservationDate BETWEEN '01/22/2020' AND '05/22/2020' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/ConfirmedFirst")
+
+    /*---Confirmed cases, deaths, recovered within LAST 4 months---*/
+    spark.sql("SELECT DISTINCT COUNT(Country_Region) AS Countries,SUM(Confirmed) AS Confirmed_Cases,SUM(Deaths) AS Deaths,SUM(Recovered) AS Recovered FROM covid19data WHERE ObservationDate BETWEEN '01/02/2021' AND '05/02/2021' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/ConfirmedLast")
+
+    /*---Confirmed in China vs rest of the world---*/
+    spark.sql("SELECT SUM(Confirmed)as China_vs_World from covid19data where Country_Region == 'Mainland China' AND Last_Update='2021-05-03 04:20:39' union SELECT SUM(Confirmed)as not from covid19data where Country_Region != 'Mainland China' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/ConfirmedChina")
+    val countries = spark.sql("SELECT DISTINCT COUNT(Country_Region) FROM covid19data WHERE Country_Region!='%China%'").head().getLong(0)
+    val confirmed = spark.sql("SELECT SUM(Confirmed) FROM covid19data WHERE Last_Update='2021-05-03 04:20:39'").head().getDouble(0)
+    val chinaCases = 1 - ((countries / confirmed) * 100)
+    println(f"China had ${chinaCases}%.0f%% of worldwide confirmed cases")
+
+    /*---Top 10 recovered across countries---*/
+    spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Recovered) AS Recovered FROM covid19data WHERE Last_Update='2021-05-03 04:20:39' GROUP BY Country_Region ORDER BY Recovered DESC LIMIT 10").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/TopRecovered")
+
+    /*---Bottom 10 recovered across countries---*/
+    spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Recovered) AS Recovered FROM covid19data WHERE Recovered>0 AND Last_Update='2021-05-03 04:20:39' GROUP BY Country_Region ORDER BY Recovered ASC LIMIT 10").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/BottomRecovered")
+
+    /*---Top 10 deaths across countries---*/
+    spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Deaths) AS Deaths FROM covid19data WHERE Last_Update='2021-05-03 04:20:39' GROUP BY Country_Region ORDER BY Deaths DESC LIMIT 10").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/TopDeaths")
+
+    /*---Bottom 10 deaths across countries---*/
+    spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Deaths) AS Deaths FROM covid19data WHERE Deaths>0 AND Last_Update='2021-05-03 04:20:39' GROUP BY Country_Region ORDER BY Deaths ASC LIMIT 10").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").csv("src/main/Data/BottomDeaths")
+  }
 }
 
