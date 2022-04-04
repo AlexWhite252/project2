757ec6ef7bc4f37db5ee00f5759128ba49897d3f
Savannah Jones <savannah350@revature.net>
2022-04-04 16:29:16 -0400

    updated confirmed in china v other countries

--- src/main/scala/project2/SparkQueries.scala
+++ src/main/scala/project2/SparkQueries.scala
@@ -31,7 +31,9 @@ class SparkQueries(spark:SparkSession) {
   }
   def confirmedChina(): Unit = {
     /*---Confirmed in China vs rest of the world---*/
-    spark.sql("SELECT SUM(Confirmed)as China_vs_World from covid19data where Country_Region == 'Mainland China' AND Last_Update='2021-05-03 04:20:39' union SELECT SUM(Confirmed)as not from covid19data where Country_Region != 'Mainland China' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("data/ConfirmedChina")
+    spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Confirmed) AS Confirmed from covid19data where Country_Region == 'Mainland China' AND Last_Update='2021-05-03 04:20:39' GROUP BY Country UNION SELECT DISTINCT Country_Region as Country,SUM(Confirmed)as not from covid19data where Country_Region != 'Mainland China' AND Last_Update='2021-05-03 04:20:39' GROUP BY Country").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("src/main/Data/ConfirmedChina")
+    spark.sql("SELECT DISTINCT Country_Region AS Country,SUM(Confirmed) AS Confirmed from covid19data where Country_Region == 'Mainland China' AND Last_Update='2021-05-03 04:20:39' GROUP BY Country UNION SELECT DISTINCT COUNT(Country_Region) as Country,SUM(Confirmed)as not from covid19data where Country_Region != 'Mainland China' AND Last_Update='2021-05-03 04:20:39'").write.option("delimiter", ",").option("header", "true").option("inferSchema", "true").mode("overwrite").csv("src/main/Data/ConfirmedChinaVsCount")
+
     val countries = spark.sql("SELECT DISTINCT COUNT(Country_Region) FROM covid19data WHERE Country_Region!='%China%'").head().getLong(0)
     val confirmed = spark.sql("SELECT SUM(Confirmed) FROM covid19data WHERE Last_Update='2021-05-03 04:20:39'").head().getDouble(0)
     val chinaCases = 1 - ((countries / confirmed) * 100)
